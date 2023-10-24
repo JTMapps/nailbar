e54276d08e671d309ed001295954b507859b0399
JTMapps <mmaponya08@gmail.com>
2023-10-24 22:01:13 +0200

    change to firebase setup, preying for the best

--- js/auth.js
+++ js/auth.js
@@ -1,38 +1,3 @@
-	// Import the functions you need from the SDKs you need
-	import{initializeApp} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-	import{getAnalytics} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
-	var getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut = require ("https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js");
-	
-	
-	// TODO: Add SDKs for Firebase products that you want to use	   
-	
-import{ getDatabase, set, get, update, remove, ref, child}from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js"
-
-// FIREBASE OBJECT CONFIGURATION;
-document.addEventListener("DOMContentLoaded", event => { 
-
-	// Your web app's Firebase configuration
-	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-	const firebaseConfig = {
-	apiKey: "AIzaSyA4xMNCyjxbhux_hBDe-nO9pF4uDsiipaI",
-	authDomain: "salon-be3a1.firebaseapp.com",
-	projectId: "salon-be3a1",
-	storageBucket: "salon-be3a1.appspot.com",
-	messagingSenderId: "425524125484",
-	appId: "1:425524125484:web:64dbba0782b46ec4d29689",
-	measurementId: "G-FHGB2XZM6D"
-	} //from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-	
-	
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-
-
-const db = getDatabase();
-
-
-
 const authForm = document.querySelector("#authForm");
 const userEmail = document.querySelector("#userEmail");
 const userPassword = document.querySelector("#userPassword");
@@ -49,8 +14,6 @@ const userPhoneNumber = document.querySelector("#userPhoneNumber");
 const business = document.querySelector("#business");
 
 //secretContent.style.display = 'none';
-});
-
 		const userSignUp = async() => {
 			const signUpEmail = userEmail.value;
 			const signUpPassword = userPassword.value;
--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function myFunction() {
  
   // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.signInBtn')) {
+    if (!event.target.matches('.dropdownBtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
