bd8bc84866eee641f4ef591afc53249f7240a04a
JTMapps <mmaponya08@gmail.com>
2023-11-02 15:29:57 +0200

    change import to require

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,4 @@
-import{initializeApp} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import{getAuth, signInWithEmailAndPassword, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import{ getDatabase, set, get, update, remove, ref, child}from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
-
+var firebase= require("firebase/app");
 
 const firebaseConfig = {
     apiKey: "AIzaSyAi7vWrdz6NCnFlnuY3KT77lB3Xw8Lg1Hc",
@@ -9,16 +6,19 @@ const firebaseConfig = {
     projectId: "nailbar-bc5e8",
     storageBucket: "nailbar-bc5e8.appspot.com",
     messagingSenderId: "672707669091",
-    appId: "1:672707669091:web:c770672694748a01f75c56"
+    appId: "1:672707669091:web:c770672694748a01f75c56",
+    databaseURL: "https://nailbar-bc5e8-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }; //"https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase();
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.getAuth(app);
+const db = firebase.getDatabase(app);
+
+
 
 const checkAuthState = async() => {
-    onAuthStateChanged(auth, user => {
+    firebase.onAuthStateChanged(auth, user => {
     if(user) {
         alert("you are already signed in");
         location.href("main.html");
@@ -37,9 +37,10 @@ checkAuthState();
 const userSignIn = async() => {
 let signInEmail = $("#userEmail").val();
 let signInPassword = $("#userPassword").val();
-signInWithEmailAndPassword(auth, signInEmail, signInPassword)
+firebase.signInWithEmailAndPassword(auth, signInEmail, signInPassword)
 .then((userCredential) => {
 const user = userCredential.user;
+
 console.log("New User: "+ user + signInEmail + signInPassword);
 alert("You have signed in successfully!");
 })
