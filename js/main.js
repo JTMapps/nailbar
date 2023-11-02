48ba94c7fa252dd9efec5ac2d2201e0145a41724
JTMapps <mmaponya08@gmail.com>
2023-11-02 14:47:26 +0200

    dropdown button change to jquery

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
  
-  // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropdownBtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  // Close the drop opdown menu if the user clicks outside of it
+$("dropdownBtn").click(function(event) {
+  if (!event.target.matches('.dropdownBtn')) {
+    var dropdowns = document.querySelectorAll("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-}
+  }
+}); 
